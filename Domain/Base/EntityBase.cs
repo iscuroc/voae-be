feafd1330b52bb3fee3aaedb9063ce6673250177
Andrea A <apalvaradol@outlook.com>
2024-06-23 20:22:46 -0600

    Add: Db config and table creation

---
+++ Domain/Base/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Base;
+
+public interface IEntityBase
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
+    
+    
+public abstract class EntityBase : IEntityBase
+{
+    public int Id { get; set; }
+    public DateTime CreatedAt { get; set; } 
+    public DateTime? UpdatedAt { get; set; }
+}
--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,8 +1,37 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Base;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure;
 
-public class ApplicationDbContext(DbContextOptions options): DbContext(options)
+public class ApplicationDbContext(DbContextOptions options) : TablesRoot(options)
 {
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var utcNow = DateTime.UtcNow;
+        ChangeTracker.DetectChanges();
+
+        foreach (var entry in ChangeTracker.Entries<IEntityBase>())
+        {
+            if (entry.State is EntityState.Detached or EntityState.Unchanged or EntityState.Deleted)
+                continue;
+            
+            switch (entry.State)
+            {
+                case EntityState.Modified:
+                    entry.Entity.UpdatedAt = utcNow;
+                    break;
+                case EntityState.Added:
+                    entry.Entity.CreatedAt = utcNow;
+                    break;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
