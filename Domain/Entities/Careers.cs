4bedaf6fef61b0798373eef00164e8fa7fa2a8bf
CarloossRomeroo <carlosfabricioromero@gmail.com>
2024-06-24 20:40:08 -0600

    Add : Tabla Carreras

---
+++ Domain/Entities/Careers.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Base;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Careers : EntityBase
+{
+    public required string Names { get; set; }
+    public required string Lastname { get; set; }
+    public required string Description { get; set; }
+    public int FacultyId { get; set; }
+    public int CoordinatorId { get; set; }
+}
