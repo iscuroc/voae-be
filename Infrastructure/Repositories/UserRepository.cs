7d2e06701f5826d769e906ec9e7924d614972729
Andrea Alvarado <95729212+annxpx@users.noreply.github.com>
2024-06-26 16:20:48 -0600

    User repository (#5)
    *Add: User repository

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository(ApplicationDbContext context) : IUserRepository
+{
+    public async Task<User?> GetUserByEmailAsync(string email)
+    {
+        var user = await context.Users
+            .Where(u => u.Email == email)
+            .FirstOrDefaultAsync();
+
+        return user;
+    }
+}
