b07a4fe167bd32812ba685535871a1ee928bb562
EDUARDJ1001 <jeduard7030@gmail.com>
2024-06-26 09:17:42 -0600

    Fix: Implement Career and Status to TablesRoot

---
+++ Domain/Entities/Career.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Base;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Career : EntityBase
+{
+    //Cambios realizados: Carlos Romero
+    public required string Name { get; set; }
+    public int FacultyId { get; set; }
+    public int CoordinatorId { get; set; }
+}
--- Domain/Entities/Status.cs
+++ Domain/Entities/Status.cs
@@ -5,5 +5,7 @@ namespace Domain.Entities;
 
 public class Status : EntityBase
 {
+    //Tabla implementada en TablesRoot
+
     public required string Name { get; set; }
 }
