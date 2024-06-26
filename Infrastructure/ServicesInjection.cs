8f80708a89980e72dc823cc99503e7cc6ade536f
Andrea A <apalvaradol@outlook.com>
2024-06-26 16:19:44 -0600

    Add: User repository

--- Infrastructure/ServicesInjection.cs
+++ Infrastructure/ServicesInjection.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using Application.Contracts;
-using Infrastructure.Authentication;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
