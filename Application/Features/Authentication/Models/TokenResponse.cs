116ad69c8413cea9bce1437e350cd81468b45931
Andrea A <apalvaradol@outlook.com>
2024-06-26 22:05:16 -0600

    Add: Login Endpoint

---
+++ Application/Features/Authentication/Models/TokenResponse.cs
@@ -0,0 +1,5 @@
+﻿using Domain.Enums;
+
+namespace Application.Features.Authentication.Models;
+
+public record TokenResponse(string Email, Role Role, string AccessToken);
---
+++ Domain/Errors/AuthenticationErrors.cs
@@ -0,0 +1,12 @@
+﻿using Shared;
+
+namespace Domain.Errors;
+
+public static class AuthenticationErrors
+{
+    public static Error InvalidCredentials => Error.Unauthorized(
+        "Authentication.InvalidCredentials",
+        "Invalid credentials"
+    );
+    
+}
