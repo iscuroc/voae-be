8aa5b98c4b8b5bf86df69b7f66448bbcf687f19c
RafaRamos31 <rafaelramos19972@gmail.com>
2024-06-26 21:16:40 -0600

    Add: Activities Table

---
+++ Domain/Entities/Activity.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Base;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Activity : EntityBase
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Location { get; set; }
+    public required string MainActivities { get; set; }
+    public required string Objectives { get; set; }
+    //public required Career CareerId { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
+    public required int TotalRooms { get; set; }
+    public int? OccupiedRooms { get; set; } = 0;
+    public string? BannerLink { get; set; }
+    //public required User SupervisorId { get; set; }
+    //public required User RequestedById { get; set; }
+    public required DateTime RequestDate { get; set; }
+    public required RequestStatus RequestStatus { get; set; } = RequestStatus.Pending;
+    //public User? ReviewedBy { get; set; }
+    public DateTime? ReviewDate { get; set; }
+    public string? ReviewObservations { get; set; }
+    public DevelopmentStatus? DevelopmentStatus { get; set; }
+
+}
