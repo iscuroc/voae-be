2476a56189f46885fbc560417cec48efa183b2be
CarloosRomeroo <75095861+CarloosRomeroo@users.noreply.github.com>
2024-07-02 16:56:45 -0600

    PC-61 (#20)
    * Add : ProfileHoursVoae
    
    * Fix : TablesRoot
    
    * Fix : TablesRoot
    
    * Add : ProfileHoursVOAE
    
    ---------
    
    Co-authored-by: CarloossRomeroo <carlosfabricioromero@gmail.com>

---
+++ Domain/Entities/ProfileHoursVOAE.cs
@@ -0,0 +1,9 @@
+using Domain.Base;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class ProfileHoursVOAE : EntityBase
+{
+    public Role Role { get; set; }
+}
--- Infrastructure/TablesRoot.cs
+++ Infrastructure/TablesRoot.cs
@@ -10,6 +10,5 @@ public abstract class TablesRoot(DbContextOptions options) : DbContext(options)
     public required DbSet<Career> Careers {get; set;}
     public required DbSet<Activity> Activities { get; set; }
     public required DbSet<ActivityHours> ActivitiesHours { get; set; }
-    public required DbSet<ActivityParticipant> ActivityParticipants {get; set;}
 
 }
