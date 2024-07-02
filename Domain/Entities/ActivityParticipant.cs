c444d1afb85e5b30c1a7ac5b63fa5edc203934f8
EDUARDJ1001 <jeduard7030@gmail.com>
2024-07-02 15:16:08 -0600

    Add ActivityParticipant Table

--- Domain/Entities/ActivityParticipant.cs
+++ Domain/Entities/ActivityParticipant.cs
@@ -7,6 +7,6 @@ public class ActivityParticipant : EntityBase
 {
     //public int ActivityId { get; set; }
     //public int ParticipantId { get; set; }
-    public ParticipationType TipoParticipante { get; set; }
-    public bool ConfirmarAsistencia { get; set; }
+    public ParticipationType ParticipantType { get; set; }
+    public bool AttendanceConfirmation { get; set; }
 }
