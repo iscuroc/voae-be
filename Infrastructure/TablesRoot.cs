067c03681ce49bbff073ad729a06ff61af43e715
Andrea A <apalvaradol@outlook.com>
2024-07-14 15:10:47 -0600

    Delete: Redundant entity

--- Infrastructure/TablesRoot.cs
+++ Infrastructure/TablesRoot.cs
@@ -9,7 +9,5 @@ public abstract class TablesRoot(DbContextOptions options) : DbContext(options)
     public required DbSet<Faculty> Faculties { get; set; }
     public required DbSet<Career> Careers {get; set;}
     public required DbSet<Activity> Activities { get; set; }
-    public required DbSet<ActivityHours> ActivitiesHours { get; set; }
-    public required DbSet<Featured> Featureds { get; set; }
-
+    public required DbSet<ActivityHour> ActivityHours { get; set; }
 }
