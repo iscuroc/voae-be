21fdf585324630f71a93449b69b04d08362ad3c7
Andrea A <apalvaradol@outlook.com>
2024-07-21 23:57:09 -0600

    Fix: compilation errors

--- Infrastructure/Services/UserNotificationService.cs
+++ Infrastructure/Services/UserNotificationService.cs
@@ -14,14 +14,15 @@ public class UserNotificationService(
         CancellationToken cancellationToken = default
     )
     {
-        var subject = "Portal Curoc - Confirme su correo electrónico";
+        const string subject = "Portal Curoc - Confirme su correo electrónico";
         var host = configuration["Frontend:Url"]!;
-        var html = $@"
-            <h1>Portal Curoc</h1>
-            <p>Por favor, confirme su correo electrónico haciendo clic en el siguiente enlace:</p>
-            <a href='{host}/confirm-email?token={token}'>Confirmar correo electrónico</a>";
+        var html = $"""
+                        <h1>Portal Curoc</h1>
+                        <p>Por favor, confirme su correo electrónico haciendo clic en el siguiente enlace:</p>
+                        <a href='{host}/confirm-email?token={token}'>Confirmar correo electrónico</a>
+                    """;
 
-        await emailSender.SendEmailAsync(to, subject, html);
+        await emailSender.SendEmailAsync(to, subject, html, cancellationToken);
     }
 
     public async Task SendResetPasswordInstructionsAsync(CancellationToken cancellationToken = default)
@@ -31,12 +32,14 @@ public class UserNotificationService(
 
     public async Task SendWelcomeEmailAsync(string to, CancellationToken cancellationToken = default)
     {
-        var subject = "Bienvenido a Portal Curoc";
-        var html = $@"
-            <h1>Bienvenido a Portal Curoc</h1>
-            <p>Gracias por registrarte. Estamos encantados de tenerte con nosotros.</p>
-            <p>Explora nuestras funciones y servicios en el siguiente enlace:</p>
-            <a href='{configuration["Frontend:Url"]}'>Ir a Portal Curoc</a>";
+        const string subject = "Bienvenido a Portal Curoc";
+        var host = configuration["Frontend:Url"]!;
+        var html = $"""
+                        <h1>Bienvenido a Portal CUROC</h1>
+                        <p>Gracias por registrarte. Estamos encantados de tenerte con nosotros.</p>
+                        <p>Explora nuestras funciones y servicios en el siguiente enlace:</p>
+                        <a href='{host}'>Ir a Portal CUROC</a>
+                    """;
 
         await emailSender.SendEmailAsync(to, subject, html, cancellationToken);
     }
