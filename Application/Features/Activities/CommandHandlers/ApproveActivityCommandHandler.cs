372391ab2f8066ad46200a15681d6d467426071e
RafaRamos31 <rafaelramos19972@gmail.com>
2024-08-12 21:15:19 -0600

    Add: Approve activity process

---
+++ Application/Features/Activities/CommandHandlers/ApproveActivityCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Application.Contracts;
+using Application.Features.Activities.Commands;
+using Domain.Contracts;
+using Domain.Enums;
+using Domain.Errors;
+using Mediator;
+using Shared;
+
+namespace Application.Features.Activities.CommandHandlers
+{
+    public record ApproveActivityCommandHandler(
+        IActivityRepository ActivityRepository,
+        IUserRepository UserRepository,
+        ICurrentUserService CurrentUserService
+    ) : ICommandHandler<ApproveActivityCommand, Result>
+    {
+        public async ValueTask<Result> Handle(ApproveActivityCommand request, CancellationToken cancellationToken)
+        {
+
+            var activity = await ActivityRepository.GetByIdAsync(request.ActivityId, cancellationToken);
+            if (activity is null) return Result.Failure(ActivityErrors.ActivityNotFound);
+
+            var currentUser = await CurrentUserService.GetCurrentUserAsync(cancellationToken);
+            if (currentUser.Role != Role.Voae) return Result.Failure(ActivityErrors.InvalidApprovalUserRole);
+
+            if (!(activity.ActivityStatus == ActivityStatus.Pending || activity.ActivityStatus == ActivityStatus.Rejected))
+                return Result.Failure(ActivityErrors.InvalidActivityStatusForApproval);
+
+            activity.ActivityStatus = ActivityStatus.Approved;
+            activity.LastReviewedAt = DateTime.UtcNow;
+            activity.ReviewerObservations = string.IsNullOrEmpty(activity.ReviewerObservations)
+                ? request.ReviewerObservation
+                : $"{activity.ReviewerObservations}|{request.ReviewerObservation}";
+            activity.ReviewedById = currentUser.Id;
+
+            await ActivityRepository.UpdateAsync(activity, cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
