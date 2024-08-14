d6c1a347133a5fd7607d1dc372af4ec00163da4a
Andrea A <apalvaradol@outlook.com>
2024-08-13 23:14:09 -0600

    Add: ActivityMember entity

---
+++ Infrastructure/Configurations/ActivityMemberConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+public class ActivityMemberConfiguration: IEntityTypeConfiguration<ActivityMember>
+{
+    public void Configure(EntityTypeBuilder<ActivityMember> builder)
+    {
+        builder.Property(am => am.ActivityId)
+            .IsRequired();
+
+        builder.Property(am => am.MemberId)
+            .IsRequired();
+
+        builder.HasOne(am => am.Activity)
+            .WithMany(a => a.Members)
+            .HasForeignKey(am => am.ActivityId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(am => am.Member)
+            .WithMany(u => u.JoinedActivities)
+            .HasForeignKey(am => am.MemberId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasMany(am => am.Scopes)
+            .WithOne(ams => ams.ActivityMember)
+            .HasForeignKey(ams => ams.ActivityMemberId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Infrastructure/Migrations/20240814051259_AddActivityMemberTable.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActivityMemberTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActivityMembers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ActivityId = table.Column<int>(type: "integer", nullable: false),
+                    MemberId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActivityMembers_Activities_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ActivityMembers_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActivityMemberScopes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MemberScope = table.Column<int>(type: "integer", nullable: false),
+                    Hours = table.Column<int>(type: "integer", nullable: false),
+                    ActivityMemberId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityMemberScopes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActivityMemberScopes_ActivityMembers_ActivityMemberId",
+                        column: x => x.ActivityMemberId,
+                        principalTable: "ActivityMembers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityMembers_ActivityId",
+                table: "ActivityMembers",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityMembers_MemberId",
+                table: "ActivityMembers",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityMemberScopes_ActivityMemberId",
+                table: "ActivityMemberScopes",
+                column: "ActivityMemberId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityMemberScopes");
+
+            migrationBuilder.DropTable(
+                name: "ActivityMembers");
+        }
+    }
+}
