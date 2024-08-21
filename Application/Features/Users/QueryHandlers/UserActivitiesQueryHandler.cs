443025e64a195ec7af219080fddf3796de3223c7
zarcolini <zavalacris799@gmail.com>
2024-08-21 12:46:37 -0600

    cambio de activity a member en query handler

--- Application/Features/Users/QueryHandlers/UserActivitiesQueryHandler.cs
+++ Application/Features/Users/QueryHandlers/UserActivitiesQueryHandler.cs
@@ -25,18 +25,18 @@ public record UserActivitiesQueryHandler(
         var userId = await GetCurrentUserIdAsync(cancellationToken);
         var user = await UserRepository.GetActivitiesAsync(userId, cancellationToken);
 
-        var activitiesResponse = user.JoinedActivities.Select(activity => new UserActivitiesResponse(
-            Id: activity.Activity.Id,
-            Name: activity.Activity.Name,
-            Description: activity.Activity.Description,
-                Scope: activity.Activity.Scopes.Select(scope => new ActivitiesScopeResponse(
+        var activitiesResponse = user.JoinedActivities.Select(member => new UserActivitiesResponse(
+            Id: member.Activity.Id,
+            Name: member.Activity.Name,
+            Description: member.Activity.Description,
+                Scope: member.Activity.Scopes.Select(scope => new ActivitiesScopeResponse(
                     ActivityScopes: scope.Scope,
                     Hours: scope.Hours
                 )).ToList(),
-            StartDate: activity.Activity.StartDate,
-            EndDate: activity.Activity.EndDate,
-            Slug: activity.Activity.Slug,
-            ActivityStatus: activity.Activity.ActivityStatus
+            StartDate: member.Activity.StartDate,
+            EndDate: member.Activity.EndDate,
+            Slug: member.Activity.Slug,
+            ActivityStatus: member.Activity.ActivityStatus
         )).ToList();
 
         return Result.Success(activitiesResponse);
