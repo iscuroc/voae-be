4ecc3168e07c8c21212bd42664aa816bdf111d72
Andrea Alvarado <95729212+annxpx@users.noreply.github.com>
2024-08-21 13:15:16 -0600

    PC-163 (#57)

---
+++ Application/Contracts/IStorageService.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Application.Contracts;
+
+public interface IStorageService
+{
+    Task<string> UploadAsync(IFormFile file, string folderName);
+}
---
+++ Infrastructure/Services/StorageService.cs
@@ -0,0 +1,12 @@
+﻿using Application.Contracts;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Services;
+
+public class StorageService : IStorageService
+{
+    public async Task<string> UploadAsync(IFormFile file, string folderName)
+    {
+        return string.Empty;
+    }
+}
