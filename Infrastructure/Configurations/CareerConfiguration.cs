d5c61b6d2595594f9f6d00150641bbb1a2a011c0
Andrea A <apalvaradol@outlook.com>
2024-08-25 22:42:59 -0600

    Add: New Careers to seeder

--- Infrastructure/Configurations/CareerConfiguration.cs
+++ Infrastructure/Configurations/CareerConfiguration.cs
@@ -26,11 +26,16 @@ public class CareerConfiguration : IEntityTypeConfiguration<Career>
         {
             new Career {Id = 1, Name = "Ingeniería en Sistemas", FacultyId = 1},
             new Career {Id = 2, Name = "Ingeniería Agroindustrial", FacultyId = 2},
-            new Career {Id = 3, Name = "Licenciatura en Comercio Internacional", FacultyId = 2},
+            new Career {Id = 3, Name = "Licenciatura en Comercio Internacional con Orientación en agroindustria", FacultyId = 2},
             new Career {Id = 4, Name = "Licenciatura en Desarrollo Local", FacultyId = 3},
             new Career {Id = 5, Name = "Licenciatura en Administración de Empresas", FacultyId = 2},
             new Career {Id = 6, Name = "Técnico en Administración de Empresas Cafetaleras", FacultyId = 2},
-            new Career {Id = 7, Name = "Técnico en Producción Agrícola", FacultyId = 1}
+            new Career {Id = 7, Name = "Técnico en Producción Agrícola", FacultyId = 1},
+            new Career {Id = 8, Name = "Licenciatura en ", FacultyId = 1},
+            new Career {Id = 9, Name = "Técnico en Microfinanzas", FacultyId = 1},
+            new Career {Id = 10, Name = "Técnico en Administración de Empresas Agropecuiarias", FacultyId = 2}
+
+
         };
         
         builder.HasData(careers);
