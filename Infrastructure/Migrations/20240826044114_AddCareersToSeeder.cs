9c65b4775ffc429ed55e85acf9b15157b657a276
Andrea Alvarado <95729212+annxpx@users.noreply.github.com>
2024-08-25 22:43:26 -0600

    Pc 166 (#61)
    * Add: File type to validator
    
    * Add: New Careers to seeder

---
+++ Infrastructure/Migrations/20240826044114_AddCareersToSeeder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCareersToSeeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Careers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Licenciatura en Comercio Internacional con Orientación en agroindustria");
+
+            migrationBuilder.InsertData(
+                table: "Careers",
+                columns: new[] { "Id", "CreatedAt", "FacultyId", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 8, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Licenciatura en ", null },
+                    { 9, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "Técnico en Microfinanzas", null },
+                    { 10, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "Técnico en Administración de Empresas Agropecuiarias", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Careers",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Careers",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Careers",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.UpdateData(
+                table: "Careers",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Licenciatura en Comercio Internacional");
+        }
+    }
+}
