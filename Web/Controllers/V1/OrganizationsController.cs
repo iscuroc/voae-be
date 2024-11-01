0938572e1facc3d163f08546d239cf7585f7b366
Erick Marley Arita <58413651+erickArita@users.noreply.github.com>
2024-11-01 10:26:41 -0600

    Pc 212 be (#80)
    * PC-212-BE: expresión regular igual que en el frontend
    
    * PC-212-BE: allow anonimos en organizaciones

--- Web/Controllers/V1/OrganizationsController.cs
+++ Web/Controllers/V1/OrganizationsController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Organizations.Models;
 using Application.Features.Organizations.Queries;
 using Mediator;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Web.Extensions;
 
@@ -9,6 +10,7 @@ namespace Web.Controllers.V1;
 public class OrganizationsController(ISender sender) : BaseController
 {
     [HttpGet]
+    [AllowAnonymous]
     [ProducesResponseType<List<OrganizationResponse>>(StatusCodes.Status200OK)]
     public async Task<IResult> GetAsync(CancellationToken cancellationToken)
     {
